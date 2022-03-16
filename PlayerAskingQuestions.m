a2083014538d38ed19db34cafd9cfd1e225e2c4b
amanda-hss <amanda_hernandez_sandate@brown.edu>
2022-03-16 11:25:15 -0400

    Main File creation, Players questions to the computer
    Written by Amanda Hernandez Sandate
    
    Duration of Work: 1.5 hours

---
+++ PlayerAskingQuestions.m
@@ -0,0 +1,60 @@
+disp('1. Is your character a male?')
+disp('2. Is your character a humanoid?')
+disp('3.Does your character have dark hair?')
+disp('4. Does your character have a head accessory?')
+disp('5. Is your character caucasian?')
+disp('6. Does your character have brown or dark eyes?')
+disp('7. Does your character possess magic or powers?')
+disp('8. Is your character primarily used for comedic relief?')
+disp('9. Is your character a side character?')
+disp('10. Does your character have a prominent love interest?')
+disp('11. Does your character have wings?')
+disp('Or type 12 to submit your final answer.')
+
+prompt = 'Choose a question by typing the number';
+PlayersChoice = input(prompt);
+
+if PlayersChoice == 1
+        ComputersAnswer = one;
+        disp(ComputersAnswer);
+elseif PlayersChoice == 2
+        ComputersAnswer = two;
+        disp(ComputersAnswer)
+elseif PlayersChoice == 3
+        ComputersAnswer = three;
+        disp(ComputersAnswer);
+elseif PlayersChoice == 4
+        ComputersAnswer = four;
+        disp(ComputersAnswer);
+elseif PlayersChoice == 5
+        ComputersAnswer = five;
+        disp(ComputersAnswer);
+elseif PLayersAnswer == 6
+        ComputersAnswer = six;
+        disp(ComputersAnswer);
+elseif PlayersChoice == 7
+        ComputersAnswer = seven;
+        disp(ComputersAnswer);
+elseif PlayersChoice == 8
+        ComputersAnswer = eight;
+        disp(ComputersAnswer);
+elseif PlayersChoice == 9
+        ComputersAnswer = nine;
+        disp(ComputersAnswer);
+elseif PlayersChoice == 10
+        ComputersAnswer = ten;
+        disp(ComputersAnswer);
+elseif PlayersChoice == 11
+        ComputersAnswer = eleven;
+        disp(ComputersAnswer);
+elseif PlayersChoice == 12
+    if FinalAnswer == ComputersCharacter
+        disp("Congratuliations, YOU WON!")
+       
+    else
+        disp("You are incorrect, I WON!")
+    end
+end
+
+
+
