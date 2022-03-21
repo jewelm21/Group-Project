15e6820aa9264b31a47c90b2cb735ccf3a5a5a44
eileenzyh <yuhan_zhang@brown.edu>
2022-03-21 04:06:28 -0400

    Debugging
    Debugging
    
    1. Changed location of the png files so the code can get access to it.
    
    2. Changes in MainFile.m
    - Deleted all except run ‘PlayerAskingQuestions.m’ and instead let the code call PlayerGridScript.m in PlayerAskingQuestions.m so that this file can call every function in there.
    
    3. Changes in PlayerAskingQuestion.m
    - Changed the entire logic of how Matlab could get access to and interact with player’s answers.
    - a=1 so the while loop could keep running before player gives an answer
    - Added ‘/n’ to prompt so that player could enter answer on the next line (UI looks better)
    - Added pause(2) so when the player ask a question the computer would pause for 2 sec before showing the next question set.
    
    NOTE THAT PlayerAskingQuestions.m  is not complete. I only worked on debugging and the logic of connecting front & back-end of the code.
    
    Coded/Debugged by Yuhan (Eileen) Zhang
    
    Time commitment: 7 hours

--- PlayerAskingQuestions.m
+++ PlayerAskingQuestions.m
@@ -1,58 +1,140 @@
-disp('1. Is your character a male?')
-disp('2. Is your character a humanoid?')
-disp('3.Does your character have dark hair?')
-disp('4. Does your character have a head accessory?')
-disp('5. Is your character caucasian?')
-disp('6. Does your character have brown or dark eyes?')
-disp('7. Does your character possess magic or powers?')
-disp('8. Is your character primarily used for comedic relief?')
-disp('9. Is your character a side character?')
-disp('10. Does your character have a prominent love interest?')
-disp('11. Does your character have wings?')
-disp('Or type 12 to submit your final answer.')
+app = PlayersGridScript;
+a = 1;
+index = randi(2);
+ans_mat = [
+    ["Cinderella" 0 1 0 1 1 0 0 0 0 1 0];
+    ["Snow White" 0 1 1 1 1 1 0 0 0 1 0];
+    [];
+    [];
+    [];
+    [];
+    [];
+    [];
+    [];
+    [];
+    [];
+    [];
+    [];
+    [];
+    [];
+    [];
+    [];
+    [];
+    [];
+    [];
+    [];
+    [];
+    [];
+    [];
+    []
+];
+while a > 0
+    disp('1. Is your character a male?')
+    disp('2. Is your character a humanoid?')
+    disp('3. Does your character have dark hair?')
+    disp('4. Does your character have a head accessory?')
+    disp('5. Is your character caucasian?')
+    disp('6. Does your character have brown or dark eyes?')
+    disp('7. Does your character possess magic or powers?')
+    disp('8. Is your character primarily used for comedic relief?')
+    disp('9. Is your character a side character?')
+    disp('10. Does your character have a prominent love interest?')
+    disp('11. Does your character have wings?')
+    disp('Or type 12 to submit your final answer.')
+    
+    prompt = 'Choose a question by typing the number\n';
+    PlayersChoice = input(prompt);
+    
+    % TODO(Eileen) Let computer randomly select a character
+    
+    % Cinderella = [0 1 0 1 1 0 0 0 0 1 0];
+    % Snowwhite = 
+    % Rapunzel = 
+    % Jasmine
+    % Merida = 
+    % Jessie = 
+    % Vidia = 
+    % Iridessa = 
+    % Rosetta = 
+    % Tinkerbell = 
+    
+%   PSUDOCODE:
+%   if PlayersChoice ~= 12
+%         judge(ans_mat(index, PlayersChoice+1))
+%     else
+%         ...
+%     end
 
-prompt = 'Choose a question by typing the number';
-PlayersChoice = input(prompt);
+% CHANGE THIS ENTIRE PART TO THE PSUDOCODE ABOVE
+    if PlayersChoice == 1
+            judge(ans_mat(index, 2));
+    elseif PlayersChoice == 2
+            ComputersAnswer = two;
+            disp(ComputersAnswer)
+    elseif PlayersChoice == 3
+            judge(ans_mat(index, 4));
+    elseif PlayersChoice == 4
+            ComputersAnswer = four;
+            disp(ComputersAnswer);
+    elseif PlayersChoice == 5
+            ComputersAnswer = five;
+            disp(ComputersAnswer);
+    elseif PlayersChoice == 6
+            ComputersAnswer = six;
+            disp(ComputersAnswer);
+    elseif PlayersChoice == 7
+            ComputersAnswer = seven;
+            disp(ComputersAnswer);
+    elseif PlayersChoice == 8
+            ComputersAnswer = eight;
+            disp(ComputersAnswer);
+    elseif PlayersChoice == 9
+            ComputersAnswer = nine;
+            disp(ComputersAnswer);
+    elseif PlayersChoice == 10
+            ComputersAnswer = ten;
+            disp(ComputersAnswer);
+    elseif PlayersChoice == 11
+            ComputersAnswer = eleven;
+            disp(ComputersAnswer);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+    elseif PlayersChoice == 12
+        FinalAnswer = final(app);
+        a = -1;
+        app.delete()
+        if FinalAnswer == ""
+            disp("You are incorrect, I WON!")
+        elseif FinalAnswer == ans_mat(index, 1)
+            disp("Congratuliations, YOU WON!")
+        else
+            disp("You are incorrect, I WON!")
+        end
+    
+    end
+    pause(2)
+end
 
-if PlayersChoice == 1
-        ComputersAnswer = one;
-        disp(ComputersAnswer);
-elseif PlayersChoice == 2
-        ComputersAnswer = two;
-        disp(ComputersAnswer)
-elseif PlayersChoice == 3
-        ComputersAnswer = three;
-        disp(ComputersAnswer);
-elseif PlayersChoice == 4
-        ComputersAnswer = four;
-        disp(ComputersAnswer);
-elseif PlayersChoice == 5
-        ComputersAnswer = five;
-        disp(ComputersAnswer);
-elseif PLayersAnswer == 6
-        ComputersAnswer = six;
-        disp(ComputersAnswer);
-elseif PlayersChoice == 7
-        ComputersAnswer = seven;
-        disp(ComputersAnswer);
-elseif PlayersChoice == 8
-        ComputersAnswer = eight;
-        disp(ComputersAnswer);
-elseif PlayersChoice == 9
-        ComputersAnswer = nine;
-        disp(ComputersAnswer);
-elseif PlayersChoice == 10
-        ComputersAnswer = ten;
-        disp(ComputersAnswer);
-elseif PlayersChoice == 11
-        ComputersAnswer = eleven;
-        disp(ComputersAnswer);
-elseif PlayersChoice == 12
-    if FinalAnswer == ComputersCharacter
-        disp("Congratuliations, YOU WON!")
-       
+
+function choice = final(app)
+    cnt = 0;
+    if app.Cinderella.BackgroundColor == [0.9216 0.5804 0.6392]
+        choice = "Cinderella";
+        cnt = cnt+1;
+    end
+    if app.Snow_White.BackgroundColor == [0.9216 0.5804 0.6392]
+        choice = "Snow_White";
+        cnt = cnt+1;
+    end
+    if cnt ~= 1
+        choice = "";
+    end
+end
+
+function judge(i)
+    if i == "1"
+        disp("Yes")
     else
-        disp("You are incorrect, I WON!")
+        disp("No")
     end
 end
 
